0d032d87a9b587817b436e7f582b0a5093e9f1fa
Icon <phamtrongtrung12699@gmail.com>
2022-08-09 21:45:33 +0700

    create nestjs-api-tutorial

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  signup() {
+    return { msg: 'i have signed up' };
+  }
+  signin() {
+    return { msg: 'i have signed in' };
+  }
+}
